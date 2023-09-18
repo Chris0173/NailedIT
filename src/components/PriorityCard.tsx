8f6ab45848197723fe7caa497c44ac9d18f393fc
chris0173 <christopherlacock@gmail.com>
2023-09-19 00:49:32 +1200

    Priority delete functionality

--- src/components/PriorityCard.tsx
+++ src/components/PriorityCard.tsx
@@ -1,12 +1,21 @@
 import React from "react";
-import { Box, Heading, Text, Badge } from "@chakra-ui/react";
+import { Box, Heading, Text, Badge, IconButton } from "@chakra-ui/react";
 import { priorityFormData } from "./PriorityForm";
+import { DeleteIcon } from "@chakra-ui/icons";
 
 interface PriorityCardProps {
   priorityData: priorityFormData;
+  onDelete: (id: number) => void;
 }
 
-const PriorityCard: React.FC<PriorityCardProps> = ({ priorityData }) => {
+const PriorityCard: React.FC<PriorityCardProps> = ({
+  priorityData,
+  onDelete,
+}) => {
+  const handleDelete = () => {
+    onDelete(priorityData.id);
+  };
+
   return (
     <Box borderWidth="1px" borderRadius="lg" p="4" m="2">
       <Heading size="md">{priorityData.priority_name}</Heading>
@@ -17,6 +26,11 @@ const PriorityCard: React.FC<PriorityCardProps> = ({ priorityData }) => {
       <Text fontSize="sm" mt="2">
         Created At: {priorityData.created_at}
       </Text>
+      <IconButton
+        aria-label="Delete"
+        icon={<DeleteIcon />}
+        onClick={handleDelete}
+      />
     </Box>
   );
 };
--- src/components/TopPriorities.tsx
+++ src/components/TopPriorities.tsx
@@ -68,6 +68,26 @@ const TopPriorities = () => {
       });
   };
 
+  const handleDeletePriority = (id: number) => {
+    fetch(`http://localhost:3001/api/priorities/${id}`, {
+      method: "DELETE",
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to delete priority");
+        }
+        return response.json();
+      })
+      .then(() => {
+        // Remove the deleted priority from the state
+        setPriorities(priorities.filter((priority) => priority.id !== id));
+        console.log("Priority deleted successfully");
+      })
+      .catch((error) => {
+        console.log("Error deleting priority:", error);
+      });
+  };
+
   return (
     <div className="cardContainer">
       <Card align="center">
@@ -87,7 +107,11 @@ const TopPriorities = () => {
             </Button>
             {isFormVisible && <PriorityForm onSubmit={handleSubmit} />}
             {priorities.map((priority, index) => (
-              <PriorityCard key={index} priorityData={priority} />
+              <PriorityCard
+                key={index}
+                priorityData={priority}
+                onDelete={handleDeletePriority}
+              />
             ))}
           </Stack>
         </CardBody>
