621affd1ca8893b5227f8af32a6784ac23c4f7a5
chris0173 <christopherlacock@gmail.com>
2023-09-18 16:17:43 +1200

    PriorityCards + Priority Fetching etc + Styling

--- src/components/AddProjectForm.tsx
+++ src/components/AddProjectForm.tsx
@@ -11,6 +11,8 @@ export interface FormData {
   title: string;
   description: string;
   address: string;
+  client_name: string;
+  client_contact_number: string;
 }
 
 const AddProjectForm: React.FC<AddProjectFormProps> = ({ onSubmit }) => {
@@ -18,6 +20,8 @@ const AddProjectForm: React.FC<AddProjectFormProps> = ({ onSubmit }) => {
     title: "",
     description: "",
     address: "",
+    client_name: "",
+    client_contact_number: "",
   });
 
   const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
@@ -32,6 +36,8 @@ const AddProjectForm: React.FC<AddProjectFormProps> = ({ onSubmit }) => {
       title: "",
       description: "",
       address: "",
+      client_name: "",
+      client_contact_number: "",
     });
   };
 
@@ -43,14 +49,17 @@ const AddProjectForm: React.FC<AddProjectFormProps> = ({ onSubmit }) => {
             <Image src={Logo} height="110px" className="image" />
             <Heading
               className="addProjectFormHeader"
-              size="md"
-              textAlign="left"
+              size="lg"
+              textAlign="center"
               mb={2}
             >
               Add Project
             </Heading>
             <Stack spacing={1}>
               <form onSubmit={handleSubmit}>
+                <Heading as="h2" size="md">
+                  Project Details
+                </Heading>
                 <input
                   className="input-field"
                   type="text"
@@ -75,6 +84,27 @@ const AddProjectForm: React.FC<AddProjectFormProps> = ({ onSubmit }) => {
                   name="address"
                   placeholder="Job address"
                   value={formData.address}
+                  onChange={handleChange}
+                  required
+                />
+
+                <Heading as="h2" size="md">
+                  Client Details
+                </Heading>
+                <input
+                  className="input-field"
+                  type="text"
+                  name="client_name"
+                  placeholder="Client Name"
+                  value={formData.client_name}
+                  onChange={handleChange}
+                />
+                <input
+                  className="input-field"
+                  type="text"
+                  name="client_contact_number"
+                  placeholder="Client Contact Number"
+                  value={formData.client_contact_number}
                   onChange={handleChange}
                   required
                 />
