df0b7fff0ca979d4102946d50e6e80581e9f16f2
chris0173 <christopherlacock@gmail.com>
2023-09-22 15:03:05 +1200

    Added Current Project Details

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -8,15 +8,26 @@ import {
   Heading,
   Text,
   IconButton,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
 } from "@chakra-ui/react";
 import "./CurrentProjects.css";
 import { DeleteIcon } from "@chakra-ui/icons";
+import { useState } from "react";
 
 export interface Project {
+  id: number;
   title: string;
   description: string;
   address: string;
-  id: number;
+  client_name: string;
+  client_contact_number: Number;
+  addition_info: string;
 }
 
 interface ProjectCardProps {
@@ -25,10 +36,20 @@ interface ProjectCardProps {
 }
 
 function ProjectCard({ project, handleDelete }: ProjectCardProps) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   const confirmDeletion = () => {
     if (window.confirm("Are you sure you want to delete this project?")) {
       handleDelete(project.id);
     }
+  };
+
+  const handleViewHere = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
   };
 
   return (
@@ -44,13 +65,38 @@ function ProjectCard({ project, handleDelete }: ProjectCardProps) {
         <Text>{project.address}</Text>
       </CardBody>
       <CardFooter justifyContent={"space-between"}>
-        <Button>View here</Button>
+        <Button onClick={handleViewHere}>View here</Button>
         <IconButton
           aria-label="Delete"
           icon={<DeleteIcon />}
           onClick={confirmDeletion}
         />
       </CardFooter>
+
+      <Modal isOpen={isModalOpen} onClose={handleCloseModal} size="xl">
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{`${project.address} Project Card`}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text className="detailsHeader">Project Details</Text>
+            <Text>Project Description:</Text>
+            <p className="detailsText">{project.description}</p>
+            <br />
+            <Text>Project Address:</Text>
+            <p className="detailsText">{project.address}</p>
+            <br />
+            <Divider />
+            <br />
+            <Text className="detailsHeader">Client Details</Text>
+            <Text>Client Name: {project.client_name}</Text>
+            <Text>Client Contact: {`${project.client_contact_number}`}</Text>
+          </ModalBody>
+          <ModalFooter>
+            <Button onClick={handleCloseModal}>Close</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Card>
   );
 }
